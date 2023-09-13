562c2f1ec284ae55e53d4ac2eba263b05f965a03
Guillaume Rand <guillaume1.rand@epitech.eu>
2023-09-13 09:51:08 +0200

    add basic Vite with three js + transfrom objects

---
+++ 03-transform-objects/src/script.js
@@ -0,0 +1,89 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// /**
+//  * Objects
+//  */
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+// Cube 1
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 2
+group.add(cube3)
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// // mesh.position.x = 0.7
+// // mesh.position.y = - 0.6
+// // mesh.position.z = 1
+// mesh.position.set(0.7, - 0.6, 1 )
+
+// scene.add(mesh)
+
+// // Scale
+// mesh.scale.set(2, 0.5, 0.5)
+
+// // Rotation 
+// // mesh.rotation.y = 3.14159 //demi-rotation (PI) use Math.PI
+// mesh.rotation.reorder('YZX') // Changes axes order ( only before the rotation)
+// mesh.rotation.y = Math.PI * 0.25
+// mesh.rotation.x = Math.PI * 0.25
+    
+
+//AxiesHelper
+const axiesHelper = new THREE.AxesHelper()
+scene.add(axiesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+// camera.position.set(1, 1, 3)
+// camera.lookAt(mesh.position)
+
+scene.add(camera)
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
