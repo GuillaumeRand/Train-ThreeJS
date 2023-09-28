735c1b6294c2b95cf52e78c799a5f003f18b953b
Guillaume Rand <71255044+GuillaumeRand@users.noreply.github.com>
2023-09-28 17:21:16 +0200

    add baked shadow and a animation with dynamique shadow to the sphere

--- 13-shadows/src/script.js
+++ 13-shadows/src/script.js
@@ -17,6 +17,15 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Textures
+ */
+
+const textureLoader = new THREE.TextureLoader()
+const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg')
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg')
+
+
+/**
  * Lights
  */
 // Ambient light
@@ -132,13 +141,29 @@ sphere.castShadow = true
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     material
-)
+    )
+
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
 
 plane.receiveShadow = true
 
 scene.add(sphere, plane)
+
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+sphereShadow.rotation.x = - Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+
+scene.add(sphereShadow)
+
+
 
 /**
  * Sizes
@@ -199,6 +224,26 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update the sphere
+    sphere.position.x = Math.cos(elapsedTime) * 1.5
+    sphere.position.z = Math.sin(elapsedTime) * 1.5
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
+
+    // Update Shadow
+    sphereShadow.position.x = sphere.position.x
+    sphereShadow.position.z = sphere.position.z
+    sphereShadow.material.opacity = (1 - sphere.position.y) * 0.3
+
+    // Update directional light
+    directionalLight.position.x = Math.sin(elapsedTime) * 2
+    directionalLight.position.y = Math.cos(elapsedTime) * 2
+    directionalLight.position.z = Math.sin(elapsedTime) * 2
+
+    // Update spotlight
+    spotLight.position.x = Math.sin(elapsedTime) * 2
+    spotLight.position.y = Math.cos(elapsedTime) * 2
+    spotLight.position.z = Math.sin(elapsedTime) * 2
 
     // Update controls
     controls.update()
