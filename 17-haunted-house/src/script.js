556fa228d4b4e482622a0d24c30dc7fb7e619ce5
Guillaume Rand <71255044+GuillaumeRand@users.noreply.github.com>
2023-09-29 11:38:58 +0200

    add graves and bushes, change the lights

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -54,6 +54,51 @@ door.position.y = 1
 door.position.z = 2 + 0.01
 scene.add(door)
 
+//Bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({color: '#89c854',})
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(- 0.8, 0.1, 2.2)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.25, 0.25, 0.25)
+bush4.position.set(- 1.3, 0.05, 2.15)
+
+house.add(bush1, bush2, bush3, bush4)
+
+// Graves
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeometry = new THREE.BoxGeometry(0.5, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({color: '#b2b6b1'})
+
+for (let i = 0; i < 50; i++) 
+{
+const angle = Math.random() * Math.PI * 2 // random angle
+const radius = 3 + Math.random() * 6 // random radius
+const x = Math.sin(angle) * radius
+const z = Math.cos(angle) * radius
+
+const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+grave.position.set(x, 0.3, z)
+grave.rotation.y = (Math.random() - 0.5) * 0.4
+grave.rotation.z = (Math.random() - 0.5) * 0.4
+
+graves.add(grave)
+
+}
+
 // Temporary sphere
 // const sphere = new THREE.Mesh(
 //     new THREE.SphereGeometry(1, 32, 32),
@@ -75,12 +120,12 @@ scene.add(floor)
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.5)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
+const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.5)
 moonLight.position.set(4, 5, - 2)
 gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
